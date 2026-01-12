81ab506a69117271880e5506f4d3e8e046c404f0
Bruno <filobruno2@gmail.com>
2026-01-12 11:36:20 +0100

    refactor form handling and enhance styling withimproved layout

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-const form = document.getElementById('form');
-
-    form.addEventListener('submit', function(event) {
-      event.preventDefault(); 
-
-      try {
+const form = document.querySelector('form');
+    
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        
         const formData = new FormData(form);
-
-        console.log("Form Data:");
         for (const [key, value] of formData.entries()) {
           console.log(`${key}: ${value}`);
         }
-      } catch (error) {
-        console.error("Error reading form data:", error);
-      }
     });
